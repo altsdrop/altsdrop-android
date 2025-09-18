a0027306d6054262b708627b0c59ce8ee2510819
jaiveer.singh <jaiveer.ie@gmail.com>
2025-09-18 17:04:25 +0100

    Release 1.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.altsdrop.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 11
-        versionName = "1.1.0"
+        versionCode = 12
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
